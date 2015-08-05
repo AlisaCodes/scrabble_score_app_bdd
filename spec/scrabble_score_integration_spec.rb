dce5c0eb91e0e581eeb79181c7c1bb0e12364b93
Guest User <student@epicodus.com>
2015-08-05 11:58:39 -0700

    adding layout, index, aand score erb

---
+++ spec/scrabble_score_integration_spec.rb
@@ -0,0 +1,13 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('the scrabble score user path', {:type => :feature}) do
+  it('processes the user entry and returns it with a number value') do
+      visit('/')
+      fill_in('word', :with => 'a')
+      click_button('Send')
+      expect(page).to have_content('1')
+    end
+  end
