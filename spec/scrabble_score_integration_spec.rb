54d2c9eff9df0b83abcffa9105616f6f353bde21
Guest User <student@epicodus.com>
2015-08-05 13:56:15 -0700

    Finally passed last spec

--- spec/scrabble_score_integration_spec.rb
+++ spec/scrabble_score_integration_spec.rb
@@ -3,11 +3,11 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('the scrabble score user path', {:type => :feature}) do
+describe('the scrabble score path', {:type => :feature}) do
   it('processes the user entry and returns it with a number value') do
-      visit('/')
-      fill_in('word', :with => 'a')
-      click_button('Send')
-      expect(page).to have_content('1')
-    end
+    visit('/')
+    fill_in('word', :with => "a")
+    click_button('Send')
+    expect(page).to have_content('1')
   end
+end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -5,7 +5,7 @@ require('scrabble_score')
 describe("String#scrabble_score") do
 
   it("returns a scrabble score for a letter") do
-    expect("a".scrabble_score()).to(eq(1))
+    expect(("a").scrabble_score()).to(eq(1))
   end
   it("returns a scrabble score for an entire word") do
     expect("cat".scrabble_score()).to(eq(5))
